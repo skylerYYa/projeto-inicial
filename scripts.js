1ef5bdbbb33a47fe9d9293ffc1b930e8e3d2dae2
skylerYYa <skelanimalskitti@gmail>
2024-03-15 08:24:10 -0300

    variavel nome

--- scripts.js
+++ scripts.js
@@ -1,8 +1,13 @@
 var botao = document.getElementById("botao");
 botao.addEventListener("click", alertar, false);
+var nome = document.getElementById("nome");
 
 function alertar(event){
-    alert("Voce clicou no botao!!!" + event);
+    alert("Voce clicou no botao!!!" + " " + nome.value);
+    
+    var numero = 7;
+    var resultado = numero % 2;
+    if(resultado == 0){
+        alert("este número é par!");
+    }
 }
-
-
